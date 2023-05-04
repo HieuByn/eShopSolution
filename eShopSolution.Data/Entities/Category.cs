8466f551a03249b7b16f9af53ffbf7a4385e83df
HieuByn <byn1612@gmail.com>
2023-05-04 21:33:42 +0700

    Add Entity

---
+++ eShopSolution.Data/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using eShopSolution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Category
+    {
+        public int Id { set; get; }
+        public int SortOrder { get; set; }
+        public bool IsShowOnHome { get; set; }
+        public int? ParentId { get; set; }
+        public Status Status { get; set; }
+    }
+}
