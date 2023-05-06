61120afb439acba26106cd8ed991a4c218d29a86
HieuByn <byn1612@gmail.com>
2023-05-06 11:16:01 +0700

    seeding database

--- eShopSolution.Data/Configurations/ProductInCategoryConfiguration.cs
+++ eShopSolution.Data/Configurations/ProductInCategoryConfiguration.cs
@@ -11,11 +11,11 @@ namespace eShopSolution.Data.Configurations
     {
         public void Configure(EntityTypeBuilder<ProductInCategory> builder)
         {
-            builder.HasKey(t => new { t.CategroyId, t.ProductId});
+            builder.HasKey(t => new { t.CategoryId, t.ProductId});
 
             builder.ToTable("ProductInCategories");
             builder.HasOne(t => t.Product).WithMany(pc => pc.ProductInCategories).HasForeignKey(pc => pc.ProductId);
-            builder.HasOne(t => t.Category).WithMany(pc => pc.ProductInCategories).HasForeignKey(pc => pc.CategroyId);
+            builder.HasOne(t => t.Category).WithMany(pc => pc.ProductInCategories).HasForeignKey(pc => pc.CategoryId);
         }
     }
 }
