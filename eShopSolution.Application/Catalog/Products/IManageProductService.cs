47f568fa11ce69bdb91657fbf2fdcc5c98f7a890
HieuByn <byn1612@gmail.com>
2023-05-06 19:26:22 +0700

    Commit application layer

---
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,18 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IManageProductService
+    {
+        Task<int> Create(ProductCreateRequestDto request);
+        Task<int> Update(ProductEditRequestDto request);
+        Task<int> Delete(int productId);
+        Task<List<ProductViewModelDto>> GetAll();
+        Task<PagedViewModel<ProductViewModelDto>> GetAllPaging(string keyword, int pageIndex, int pageSize);
+    }
+}
---
+++ eShopSolution.Application/Dtos/PagedViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Dtos
+{
+    public class PagedViewModel<T>
+    {
+        List<T> Items { set; get; }
+        public int ToTalRecord { set; get; }
+    }
+}
